4671667806ce3e5e2d204bf7edaeb3d5d01f7bea
Brasco <a.braschi90@gmail.com>
2018-07-17 14:56:04 +0200

    exception handling.

--- googlesearch/__init__.py
+++ googlesearch/__init__.py
@@ -506,12 +506,16 @@ def apis_customsearch(query, key, cx, num=None, domains=None, extra_params={}):
                     else:
                         sys.exit(1)
 
-                for item in response['items']:
-                    item_url = urlparse(item['link'])
-                    if item_url.netloc not in pages:
-                        pages.add(item_url.netloc)
-                        found += 1
-                        links.append(item['link'])
+                try:
+                    for item in response['items']:
+                        item_url = urlparse(item['link'])
+                        if item_url.netloc not in pages:
+                            pages.add(item_url.netloc)
+                            found += 1
+                            links.append(item['link'])
+                except KeyError:
+                    pass
+
                 try:
                     start_index = response["queries"]["nextPage"][0]["startIndex"]
                 except KeyError:
